bccde0926a0ac0409993da7c1cf0b5b273e9dd6d
Nicolas Duwavrent <nicolasduwavrent@hotmail.fr>
2018-11-27 16:03:58 +0100

    Update assignments/Session1/publisher.py
    Ajout de lignes de test pour publisher

--- assignments/Session1/publisher.py
+++ assignments/Session1/publisher.py
@@ -29,4 +29,7 @@ channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World !')
 print ("[x] Sent 'Hello World'")
+print ("[x] Sent 'Bonjour monde'")
+print ("[x] Sent 'Good morning city'")
+print ("[x] Sent 'hey hi hello World'")
 connection.close()
